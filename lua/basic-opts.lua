4d6aacff7b3c10a8a1a8bfb56e36d038c8365d6c
Zoheb Shujauddin <zoheb2424@gmail.com>
2026-04-26 08:39:49 +0530

    moves keybindings over to plugin; makes different kinds of whitespace visible

--- lua/basic-opts.lua
+++ lua/basic-opts.lua
@@ -6,3 +6,34 @@ vim.g.maplocalleader = '\\'
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Highlight cursor line, and keep 4 rows above and
+-- below it
+vim.opt.cursorline = true
+vim.opt.scrolloff = 4
+
+-- Show whitespace characters
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "> ",
+    trail = "-",
+    leadmultispace = ".   ",
+    leadtab = "> >",
+}
+
+-- Tabs now produce spaces
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+
+-- Prompt save if quitting without saving
+vim.opt.confirm = true
+
+-- Highlight when yanking (copying) text.  Yanked directly from
+-- Neovim's example_init.lua
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,4 +1,63 @@
 return {
-	"mfussenegger/nvim-dap",
+    -- Plugin that will implement the debug adapter protocol client
+    "mfussenegger/nvim-dap",
+
+    keys = {
+
+        {
+            "<F5>", -- When this is pressed (in (n)ormal mode) ...
+            function() require('dap').continue() end, -- ... do this
+            desc = "Continue/Start Debug Session",
+        },
+
+        {
+            "<F10>",
+            function() require('dap').step_over() end,
+            desc = "Next Instruction",
+        },
+
+        {
+            "<F11>",
+            function() require('dap').step_into() end,
+            desc = "Step Into",
+        },
+
+        {
+            "<F12>",
+            function() require('dap').step_out() end,
+            desc = "Step Out",
+        },
+
+        {
+            "<leader>db",
+            function() require('dap').toggle_breakpoint() end,
+            desc = "Set/Erase Breakpoint",
+        },
+
+        {
+            "<leader>dr",
+            function() require('dap').repl.open() end,
+            desc = "Open Debug REPL",
+        },
+
+        {
+            "<leader>dR",
+            function() require('dap').restart() end,
+            desc = "Restart Debug Session",
+        },
+
+        {
+            "<leader>d<C-o>",
+            function() require('dap').down() end,
+            desc = "Go Down One Stack Frame",
+        },
+
+        {
+            "<leader>d<C-i>",
+            function() require('dap').up() end,
+            desc = "Go Up One Stack Frame",
+        },
+
+    },
 }
 
