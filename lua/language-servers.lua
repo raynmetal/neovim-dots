7f758efab875e066ceada7736553e76227a7d17e
Zoheb Shujauddin <zoheb.shujauddin@stryker.com>
2026-04-27 10:15:51 +0530

    reindents with spaces

--- lua/language-servers.lua
+++ lua/language-servers.lua
@@ -4,18 +4,18 @@ vim.lsp.enable('vscode-json-language-server')
 vim.lsp.enable('neocmakelsp')
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	desc = 'LSP Actions',
-	callback = function(args)
+    desc = 'LSP Actions',
+    callback = function(args)
 
-		-- Show function description on Shift+K in normal mode
-		vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { noremap = true, silent = true })
-		-- Jump to symbol definition
-		vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { noremap = true, silent = true })
-	end,
+        -- Show function description on Shift+K in normal mode
+        vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { noremap = true, silent = true })
+        -- Jump to symbol definition
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+    end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { '<filetype>' },
-	callback = function() vim.treesitter.start() end,
+    pattern = { '<filetype>' },
+    callback = function() vim.treesitter.start() end,
 })
 
